1c477027a5f96ed647bdd3d604e96f84c694776b
Bryan Worrell <bworrell@mitre.org>
2015-06-02 09:55:17 -0400

    Updated openioc_to_stix module. * Use argparse for argument parsing. * Use stix.utils.silence_warnings decorator for warning   suppression. * Broke out main() block into mulitple functions. * Removed unnecessary try/except blocks. * Use context manager for file opening/writing.

--- openioc_to_stix.py
+++ openioc_to_stix.py
@@ -1,28 +1,38 @@
 #/usr/bin/env python
-
 # Copyright (c) 2015, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
+"""
+OpenIOC to STIX Script
+Wraps output of OpenIOC to CybOX Script
+"""
 
-# OpenIOC to STIX Script
-# Wraps output of OpenIOC to CybOX Script
-# v0.13
-
+# builtin
 import sys
-import os
-import traceback
-import warnings
-import openioc #OpenIOC Bindings
-import openioc_to_cybox #OpenIOC to CybOX Script
-from cybox.core import Observables
-import stix.utils
+import argparse
+
+# openioc bindings and utilities
+import openioc
+import openioc_to_cybox
+
+# python-stix
+from stix import utils
 from stix.indicator import Indicator
 from stix.core import STIXPackage, STIXHeader
+from stix.common import InformationSource
+from stix.common.vocabs import PackageIntent
 
-__VERSION__ = 0.13
+# python-cybox
+from cybox.core import Observables
+from cybox.common import ToolInformationList, ToolInformation
 
-USAGE_TEXT = """
+
+__version__ = "0.13"
+
+
+USAGE_TEXT = \
+"""
 OpenIOC --> STIX Translator
-v0.13 BETA // Compatible with STIX v1.1.1 and CybOX v2.1
+v%s // Compatible with STIX v1.2 and CybOX v2.1
 
 Outputs a STIX Package with one or more STIX Indicators containing 
 CybOX Observables translated from an input OpenIOC XML file. 
@@ -30,74 +40,96 @@ CybOX Observables translated from an input OpenIOC XML file.
 Usage: python openioc_to_stix.py -i <openioc xml file> -o <stix xml file>
 """
 
-#Print the usage text
 def usage():
     print USAGE_TEXT
     sys.exit(1)
 
+
+def get_arg_parser():
+    desc = "OpenIOC to STIX v%s" % __version__
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument(
+        "-i",
+        required=True,
+        dest="infile",
+        help="Input OpenIOC XML filename"
+    )
+
+    parser.add_argument(
+        "-o",
+        required=True,
+        dest="outfile",
+        help="Ouput STIX XML filename"
+    )
+
+    return parser
+
+
+@utils.silence_warnings
+def write_package(package, outfn):
+    with open(outfn, "w") as f:
+        xml = package.to_xml(ns_dict={"http://openioc.org/":"openioc"})
+        f.write(xml)
+
+
+def observable_to_indicator(observable):
+    # Build CybOX tool content
+    tool = ToolInformation(tool_name='OpenIOC to STIX Utility')
+    tool.version = __version__
+
+    # Build Indicator.producer contents
+    producer = InformationSource()
+    producer.tools = ToolInformationList(tool)
+
+    # Build Indicator
+    indicator = Indicator(title="CybOX-represented Indicator Created from OpenIOC File")
+    indicator.producer = producer
+    indicator.add_observable(observable)
+
+    return indicator
+
+
 def main():
-    infilename = ''
-    outfilename = ''
+    # Parse command line arguments
+    argparser = get_arg_parser()
+    args = argparser.parse_args()
 
-    #Get the command-line arguments
-    args = sys.argv[1:]
-    
-    if len(args) < 4:
-        usage()
-        sys.exit(1)
-        
-    for i in range(0,len(args)):
-        if args[i] == '-i':
-            infilename = args[i+1]
-        elif args[i] == '-o':
-            outfilename = args[i+1]
-    if os.path.isfile(infilename): 
-        try:
-            # Perform the translation using the methods from the OpenIOC to CybOX Script
-            openioc_indicators = openioc.parse(infilename)
-            observables_obj = openioc_to_cybox.generate_cybox(openioc_indicators, infilename, True)
-            observables_cls = Observables.from_obj(observables_obj)
+    # Create OpenIOC binding object
+    openioc_indicators = openioc.parse(args.infile)
 
-            # Set the namespace to be used in the STIX Package
-            stix.utils.set_id_namespace({"https://github.com/STIXProject/openioc-to-stix":"openiocToSTIX"})
+    # Create CybOX Observables bindings from OpenIOC binding object
+    observables_obj = openioc_to_cybox.generate_cybox(
+        indicators=openioc_indicators,
+        infilename=args.infile,
+        embed_observables=True
+    )
 
-            # Wrap the created Observables in a STIX Package/Indicator
-            stix_package = STIXPackage()
-            # Add the OpenIOC namespace
-            input_namespaces = {"http://openioc.org/":"openioc"}
-            stix_package.__input_namespaces__ = input_namespaces
+    # Create Observables from binding object
+    observables = Observables.from_obj(observables_obj)
 
-            for observable in observables_cls.observables:
-                indicator_dict = {}
-                producer_dict = {}
-                producer_dict['tools'] = [{'name':'OpenIOC to STIX Utility', 'version':str(__VERSION__)}]
-                indicator_dict['producer'] = producer_dict
-                indicator_dict['title'] = "CybOX-represented Indicator Created from OpenIOC File"
-                indicator = Indicator.from_dict(indicator_dict)
-                indicator.add_observable(observables_cls.observables[0])
-                stix_package.add_indicator(indicator)
+    # Build Indicators from the Observable objects
+    indicators = [observable_to_indicator(o) for o in observables]
 
-            # Create and write the STIX Header
-            stix_header = STIXHeader()
-            stix_header.package_intent = "Indicators - Malware Artifacts"
-            stix_header.description = "CybOX-represented Indicators Translated from OpenIOC File"
-            stix_package.stix_header = stix_header
+    # Set the namespace to be used in the STIX Package
+    utils.set_id_namespace({"https://github.com/STIXProject/openioc-to-stix":"openiocToSTIX"})
 
-            # Write the generated STIX Package as XML to the output file
-            outfile = open(outfilename, 'w')
-            # Ignore any warnings - temporary fix for no schemaLocation w/ namespace
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                outfile.write(stix_package.to_xml())
-                warnings.resetwarnings()
-            outfile.flush()
-            outfile.close()
-        except Exception, err:
-            print('\nError: %s\n' % str(err))
-            traceback.print_exc()
-    else:
-        print('\nError: Input file not found or inaccessible.')
-        sys.exit(1)
+    # Wrap the created Observables in a STIX Package/Indicator
+    stix_package = STIXPackage()
+
+    # Set the Indicators collection
+    stix_package.indicators = indicators
+
+    # Create and write the STIX Header. Warning: these fields have been
+    # deprecated in STIX v1.2!
+    stix_header = STIXHeader()
+    stix_header.package_intent = PackageIntent.TERM_INDICATORS_MALWARE_ARTIFACTS
+    stix_header.description = "CybOX-represented Indicators Translated from OpenIOC File"
+    stix_package.stix_header = stix_header
+
+    # Write the STIXPackage to a output file
+    write_package(stix_package, outfn=args.outfile)
+
 
 if __name__ == "__main__":
     main()    
